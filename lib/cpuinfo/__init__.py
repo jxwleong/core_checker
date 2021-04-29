7d318512a895c2e83428f19c6393f449ee6214c8
jxwleong <jason.xie.wei.leong@gmail.com>
2021-04-29 20:52:17 +0800

    Update the __init__.py of lib\cpuinfo to attempt to fix the import issue in Ubuntu

--- lib/cpuinfo/__init__.py
+++ lib/cpuinfo/__init__.py
@@ -1,3 +1,5 @@
+"""
+Original __init__.py
 
 import sys
 
@@ -5,5 +7,8 @@ if sys.version_info[0] == 2:
 	from cpuinfo import *
 else:
 	from cpuinfo.cpuinfo import *
+"""
+
+from cpuinfo.cpuinfo import *
 
 
