f78ef8b031779fcaeabdf379f8c6f4e7b32c51a9
Jason Xie Wei Leong <jason.xie.wei.leong@intel.com>
2021-04-30 13:17:40 +0800

    Remove dependencies on third party modules Well, the dependencies for psutil and cpuinfo had causes many issues in different system and OS. So I removed the use of them and use Python built-in and OS temrinal utilities. Currently using WMIC in WOS and the prototype is completed. Will use 'lscpu' for Linux.

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import argparse
 import os
+import re
 import platform
 import sys
+import subprocess
 import logging
+
 
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, ROOT_DIR)
 
-# Linux system comes with psutil (on Ubuntu 20.04.1 LTS)
-if platform.system() == "Windows":	 from lib import psutil
-else:	import psutil
 
-from lib import cpuinfo
+
 
 with open("core_checker.log", "w") as f:    pass
 
@@ -24,38 +24,58 @@ logging.basicConfig(
     ]
 )
 
-CPU_INFO_DICT = cpuinfo.get_cpu_info()
-cpu_name = CPU_INFO_DICT["brand_raw"]
-cpu_arch = CPU_INFO_DICT["arch"]
+
 username = os.environ.get('USERNAME')
 python_version = sys.version
 os_type = sys.platform
 os_version = platform.platform()
-physical_core = psutil.cpu_count(logical = False)
-logical_core = psutil.cpu_count(logical = True)
+
+
+def get_processer_info():
+    """
+    Get processor information in windows using WMIC.
+
+    Output example:
+    Manufacturer  Name                                      NumberOfCores  ThreadCount
+    GenuineIntel  Intel(R) Core(TM) i5-6300U CPU @ 2.40GHz  2              4
+
+    Retval:
+    List in format of
+    [Manufacturer, Name, NumberOfCores, ThreadCount]
+    """
+    info_list = []
+    output = subprocess.check_output("wmic cpu get Manufacturer,Name,ThreadCount, NumberOfCores").decode()
+    output = output.replace("\r\r", "").split("\n")[1] # Remove the \r\r then extract the second line only
+    output = re.split(r"\s{2,14}?", output)
+
+    for element in output:
+        if element != "":
+            info_list.append(element)
+        
+    return info_list
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-ct", "--core_type", help="physical(p) or logical(l), DEFAULT=logical",type=str, nargs=1, default="logical")
-parser.add_argument('-expnumc', '--expected_number_of_core', 
-                    help=f"Expected number of cores. DEFAULT=Number of logical cores ({logical_core})", 
-                    type=int,
-                    default=logical_core)
+#parser.add_argument('-expnumc', '--expected_number_of_core', 
+#                    help=f"Expected number of cores. DEFAULT=Number of logical cores ({logical_core})", 
+#                    type=int,
+#                    default=logical_core)
 
 
 args = parser.parse_args()
 print(args)
 
-logging.info("====================================")
-logging.info("         SYSTEM INFORMATION         ")
-logging.info("====================================")
-logging.info(f"CPU NAME: {cpu_name}")
-logging.info(f"CPU ARCH: {cpu_arch}")
-logging.info(f"USER: {username}")
-logging.info(f"Python version: {python_version}")
-logging.info(f"OS type: {os_type}")
-logging.info(f"OS version: {os_version}")
-logging.info(f"Number of Physical Cores: {physical_core}")
-logging.info(f"Number of Logical Cores: {logical_core}")
+#logging.info("====================================")
+#logging.info("         SYSTEM INFORMATION         ")
+#logging.info("====================================")
+#logging.info(f"CPU NAME: {cpu_name}")
+#logging.info(f"CPU ARCH: {cpu_arch}")
+#logging.info(f"USER: {username}")
+#logging.info(f"Python version: {python_version}")
+#logging.info(f"OS type: {os_type}")
+#logging.info(f"OS version: {os_version}")
+#logging.info(f"Number of Physical Cores: {physical_core}")
+#logging.info(f"Number of Logical Cores: {logical_core}")
 
 
